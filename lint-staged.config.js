bead7b9a6267f75eb58f2b7877c3dae29da539c2
Random <690497067@qq.com>
2022-07-14 11:06:21 +0800

    Update lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-//   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
-//   '**/*.ts?(x)': () => 'npm run build-types',
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
+  '**/*.ts?(x)': () => 'npm run build-types',
 //   '*.json': ['prettier --write'],
 };
